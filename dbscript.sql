3b1580ace9f789b5c8577f5937d05d68a0cdba6d
Joshua Cookhorne <joshua.cookhorne@revature.net>
2022-03-16 15:30:06 -0400

    dbscript/dockercompose

--- dbscript.sql
+++ dbscript.sql
@@ -3,12 +3,12 @@ CREATE DATABASE project_1;
 
 CREATE TABLE employee_details(employee_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY, first_name varchar(25), last_name varchar(25), address varchar(25), contact varchar(12), email varchar(25), username varchar(25), password varchar(25));
 
-ALTER TABLE employee_details add column role varchar(10);
+
 
 
 CREATE TABLE manager_details(manager_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY, first_name varchar(25), last_name varchar(25), address varchar(25), contact varchar(12), email varchar(25), username varchar(25), password varchar(25));
 
-ALTER TABLE manager_details add column role varchar(10);
+
 
 
 CREATE TABLE reimbursement_details(reimbursement_id int GENERATED ALWAYS AS IDENTITY, employee_id int, reimbursement_amount float(2), reimbursement_reason varchar(250), reimbursement_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (reimbursement_id), FOREIGN KEY(employee_id) REFERENCES employee_details(employee_id));
@@ -26,5 +26,3 @@ CREATE TABLE pending_details(pending_id int GENERATED ALWAYS AS IDENTITY, employ
 ALTER TABLE pending_details add column status varchar(10);
 
 
-
-INSERT INTO manager_details(first_name, last_name, address, contact, email, username, password) VALUES('eren', 'yeager', 'paradise island', '7058752445', 'attacktitan@gmail.com', '01', '77');
